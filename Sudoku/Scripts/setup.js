8d8685a3bc6fd208600ab984f2d4f1aaeea17a75
DairoDem <dairo_demeulemeester@hotmail.com>
2021-04-27 12:30:09 +0200

    overhaul 427

--- Sudoku/Scripts/setup.js
+++ Sudoku/Scripts/setup.js
@@ -8,7 +8,6 @@
 let board= [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],
             [0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]];
-let counter = 0;
 let squares =  document.getElementsByClassName("square");
 
 
@@ -17,79 +16,126 @@ setBoard(board);
 
 function newBoard()
 {
-    //Step1
-
-    //step2
-    for (let n = 0; n < board.length; n++) 
+    //let n = 1;
+    for (let n = 1; n < 10; n++) 
     {
-        fillNonas(n);
+        fillNonasWith(n);
     }
 
 }
-function fillNonas(nona)
-{
-    //set own nonaposibilities
-    let isfilled = true;
-    let nonaUsed = [];
-    let nonaRow = Math.floor(nona/3);
-    let nonaCol = nona%3;
-    //let x = 8;
-
-    //for every square
-    for(let x = 0; x < 9 ; x++)
+function fillNonasWith(_number)
+{     
+    for (let nona = 0; nona < board.length; nona++) 
     {
-        let nrUsedRow = [];
-        let nrUsedCol = [];
+        let availableSquares = [0,1,2,3,4,5,6,7,8];
+        let nonaRow = Math.floor(nona/3);
+        let nonaCol = nona%3;
 
-        let colStart = x % 3;
-        let rowStart = Math.floor(x/3);
-        let currAvailable = [1,2,3,4,5,6,7,8,9];
+        let sqUsed=returnFilledSquares(nona);
 
-        //check current row for used numbers   
-        for (let n = nonaRow; n < 3 + nonaRow; n++) 
+        removeFromArray(sqUsed, availableSquares);
+
+
+
+        //add restrictions (row,column) to availablelist
+        //for every available square check current row of this square.
+        let tmp = GetValues(availableSquares);
+        let currAvail = [];
+        for (let sq = 0; sq < tmp.length; sq ++) 
         {  
-            for (let sq= rowStart*3 ; sq < 3+ (rowStart*3); sq++) 
-            {  
-                if (board[n][sq] !== 0)
+            let x = availableSquares[sq];
+            if (isRowAvailable(_number, x, nonaRow)) 
+            {
+                if (isColAvailable(_number, x, nonaCol)) 
                 {
-                       nrUsedRow.push(board[n][sq]);
+                    currAvail.push(availableSquares[sq]);
                 }
             }
-         }
-         //check current col for used numbers
-          for (let n = nonaCol; n < 9; n+=3)
-          {  
-                 for (let sq = colStart; sq < 9; sq+=3) 
-                  {  
-                     if (board[n][sq] !== 0) 
-                    {
-                         nrUsedCol.push(board[n][sq]);
-                    }
-                 }
         }
+        availableSquares = GetValues(currAvail);
+
+        //----
+
+        let rndSquare = randomSquare(availableSquares);
+        board[nona][rndSquare] = _number;
+        //pick random square
+
+        //if possible insert number in square
+    }
     
-        //get possiblities from (!nona!,row,col)
-        let nrUsed = combinedArrays(nrUsedRow,nrUsedCol);
-        nrUsed = combinedArrays(nrUsed, nonaUsed);
-            
-    
-        for (let i = 0; i < nrUsed.length; i++) 
-        {
-            if (currAvailable.indexOf(nrUsed[i]) < currAvailable.length) 
+}
+function isColAvailable(_nr, _sq, _nonaCol)
+{
+    let isUnique = true;
+    let colStart = _sq%3;
+    for (let n = _nonaCol; n < 9; n+=3)
+    {  
+        for (let check = colStart; check < 9; check+=3)
+        {  
+            if (board[n][check] === _nr) 
             {
-                currAvailable.splice(currAvailable.indexOf(nrUsed[i]),1);
+                isUnique = false;
+                break;
             }
         }
-            
-        let rnd = Math.floor(Math.random()*currAvailable.length);
-    
-        let currNr = currAvailable[rnd];
-        console.log(currNr);
-        board[nona][x] = currNr;
-        nonaUsed.push(currNr);
+        if (!isUnique) 
+        {
+            break;
+        }
+    }
+    return isUnique;
+}
+function isRowAvailable(_nr, _sq, _nonaRow)
+{
+    let isUnique = true;
+    let rowStart = Math.floor(_sq/3);
 
-    }       
-    
+    //check current row for used number 
+    for (let n = _nonaRow*3; n < 3 + (_nonaRow*3); n++) 
+    {  
+        for (let check= rowStart*3 ; check < 3+ (rowStart*3); check++) 
+        {  
+            if (board[n][check] === _nr) 
+            {
+                isUnique = false;
+                break;
+            }
+        }
+        if (!isUnique) 
+        {
+            break;
+        }
+    }
+    return isUnique;
+}
+function GetValues(_arr)
+{
+    let myArr = [];
+    for (let i = 0; i < _arr.length; i++) 
+    {
+        myArr.push(_arr[i]);
+    }
+    return myArr;
+}
+function returnFilledSquares(_nona)
+{
+    let arr=[];
+    for (let sq = 0; sq < 9; sq++) 
+    {
+        if(board[_nona][sq] !== 0)
+        {
+            arr.push(sq);
+        }
+    }
+
+    return arr;
+}
+function removeFromArray(removeArr, fromArr)
+{
+    for (let i = 0; i < removeArr.length; i++) 
+    {
+        fromArr.splice(fromArr.indexOf(removeArr[i]),1);        
+    }
 }
 function combinedArrays(arr1, arr2)
 {
@@ -118,26 +164,19 @@ function combinedArrays(arr1, arr2)
     
     return arr;
 }
-function randomizeNona(nona)
+function randomSquare(arr)
 {
-    let numbers = [1,2,3,4,5,6,7,8,9];
-
-    for (let sq = 0; sq < 9; sq++) 
-    {
-        let nr = numbers[Math.floor(Math.random()*numbers.length)];
-        board[nona][sq] = nr;
-        numbers.splice(numbers.indexOf(nr),1);
-    }
-
+    return arr[Math.floor(Math.random()*arr.length)];
 }
 function setBoard(_board)
 {
+    let counter = 0;
     for (let nona = 0; nona < 9; nona++)
     {
         for(let sq = 0; sq < 9; sq++)
         {
             squares[counter].value = _board[nona][sq];
-            if (squares[counter].value != "0") 
+            if (squares[counter].value !== "0") 
             {
                 squares[counter].classList.add("closed");
             }
